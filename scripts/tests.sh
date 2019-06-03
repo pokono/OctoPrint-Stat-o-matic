6e7e30d08c83c4659b29e4987662616722e5b4b5
Ivan Carosati <itsmepokono@me.com>
2019-06-02 23:41:15 -0400

    Test 17. (yayyy!!)

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-mkdir dependencies
-cd dependencies
-git clone https://github.com/foosel/OctoPrint.git
-cd OctoPrint
-virtualenv venv
-source ./venv/bin/activate
-pip install --upgrade pip
-pip install -e .[develop,plugins]
-cd ./ && nosetests
+cd && nosetests
