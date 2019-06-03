49ef275712707f3705ca18ea4009193f0a044b0f
Ivan Carosati <itsmepokono@me.com>
2019-06-02 23:12:42 -0400

    Test 14.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+git clone https://github.com/foosel/OctoPrint.git
+cd OctoPrint
+virtualenv venv
+source ./venv/bin/activate
+pip install --upgrade pip
+pip install -e .[develop,plugins]
 cd ./ && pip install . && nosetests
