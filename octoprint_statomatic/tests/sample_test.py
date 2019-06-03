a018b822807843c719588a6ba8828d68ddb6506a
Ivan Carosati <itsmepokono@me.com>
2019-06-02 22:12:45 -0400

    Sample test.

--- octoprint_statomatic/tests/sample_test.py
+++ octoprint_statomatic/tests/sample_test.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 import octoprint_statomatic
 
 
-class TestJoke(TestCase):
+class TestSample(TestCase):
 	def test_is_string(self):
 		s = "something"
 		self.assertTrue(isinstance(s, basestring))
