705e015bb4c8386d7bd3ea2693f37531df8d12ad
Ivan Carosati <itsmepokono@me.com>
2019-06-02 23:28:21 -0400

    Test 15.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-git clone https://github.com/foosel/OctoPrint.git
-cd OctoPrint
-virtualenv venv
-source ./venv/bin/activate
-pip install --upgrade pip
-pip install -e .[develop,plugins]
-cd ./ && pip install . && nosetests
+#git clone https://github.com/foosel/OctoPrint.git
+#cd OctoPrint
+#virtualenv venv
+#source ./venv/bin/activate
+#pip install --upgrade pip
+#pip install -e .[develop,plugins]
+#cd ./ && pip install . && nosetests
+python setup.py test
