69ab412020e9c48444f15a5dd23713e90acf1ddf
Ivan Carosati <itsmepokono@me.com>
2019-06-02 23:51:10 -0400

    Test 19. (cristo santo)

--- scripts/octoprint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-mkdir dependencies
-cd dependencies
-git clone https://github.com/foosel/OctoPrint.git
-cd OctoPrint
-virtualenv venv
-source ./venv/bin/activate
-pip install --upgrade pip
-pip install -e .[develop,plugins]
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-nosetests
+mkdir dependencies
+cd dependencies
+git clone https://github.com/foosel/OctoPrint.git
+cd OctoPrint
+virtualenv venv
+source ./venv/bin/activate
+pip install --upgrade pip
+pip install -e .[develop,plugins]
+cd && nosetests
